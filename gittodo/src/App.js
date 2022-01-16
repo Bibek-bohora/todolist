a805d99b6b09f4258bfe8ae94734dca54c3fa6b5
Bibek Bohora <85892685+Bibek-bohora@users.noreply.github.com>
2022-01-16 20:32:56 +0545

    add

--- gittodo/src/App.js
+++ gittodo/src/App.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
+
+  const [name, setname] = useState("");
+
+  const events=(e)=>{
+
+
+    console.log(e.target.value);
+    setname(e.target.value);
+  }
+
+  const [sub, setsub] = useState([]);
+
+  const events1=(e)=>{
+
+console.log("click me")
+
+if(name){
+
+
+  setsub((bug)=>{
+     
+    return [...bug,name]
+  })
+}
+
+   setname("");
+  }
+
+  
+  const events2=(id)=>{
+
+    
+
+    setsub((term)=>{
+
+
+      return term.filter((tex,index)=>{
+
+        return index!==id
+      })
+    })
+  }
   return (
-    <div>git
-        <h1>this is lodolist</h1>
+    <div>
+        <h1> git this is lodolist</h1>
+            <h1></h1>
+        <input type="text" value={name} onChange={events} />
+
+        <button onClick={events1}>click me</button>
+
+        <ul>
+
+        {sub.map((uli,index)=>{
+
+
+          return <li key={index} id={index} >{uli} <span onClick={events2} style={{cursor:"pointer"}}>X</span></li>
+        }) }
+        </ul>
+
+        <Button  variant="sucess" size='10'>Button #1</Button>
+        <Button variant="primary">Primary</Button>{' '}
     </div>
   )
 }
